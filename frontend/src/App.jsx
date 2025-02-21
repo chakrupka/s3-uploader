370d3652143602bdcb43cadfcc4fb951aa4d49b9
Cha Krupka <chakrupka238@gmail.com>
2025-02-21 16:41:52 -0500

    typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Fallback from './components/Fallback';
-import NewPost from './components/newPost';
+import NewPost from './components/NewPost';
 
 const App = () => {
   return (
